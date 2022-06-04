7585ebf0ec044f959221b47d73ccd6e6c3210056
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-06-04 01:26:33 -0300

    Exercício 1 - (TDD) Desenvolva testes de no mínimo 25% de cobertura total e 100% da função fetchProducts

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,21 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', async () => {
+
+    const response = await (typeof fetchProducts === 'function');
+
+    const expected = (true);
+
+    expect(response).toEqual(expected)
+  });
+
+  it('Execute a função fetchProducts com o argumento computador e teste se fetch foi chamada', async () => {
+
+    const response = await fetchProducts('computador');
+
+    const expected = ( 'https://api.mercadolibre.com/sites/MLB/search?q=computador' );
+
+    expect(response).toEqual(expected)
+  });
 });
