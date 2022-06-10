c16a8f74c5cc7b95459742666e48ef4ed0b2aa46
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-06-10 01:16:55 -0300

    Exercício 7 - (TDD) Desenvolva testes para atingir 100% de cobertura total e 100% da função getSavedCartItems

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,5 +5,23 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
+  it('1 -Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado', () => {
+
+    const response = (getSavedCartItems());
+
+    const expected = (localStorage.getItem());
+
+    expect(response).toHaveBeenCalled(expected);
+  });
+
+  it('2 - Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o cartItems como parâmetro', () => {
+
+    const response = (getSavedCartItems());
+
+    const expected = (localStorage.getItem(cartItems));
+
+    expect(response).toHaveBeenCalledWith(expected);
+  });
+
   fail('Teste vazio');
 });
