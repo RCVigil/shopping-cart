f5658d919224a31d4f8e362082903d660400c89c
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-06-17 20:45:59 -0300

    EXERCÍCIO 9 - Calcule o valor total dos itens do carrinho de compras

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ const createCustomElement = (element, className, innerText) => {
 const itemSection = document.querySelector('.items');
 
 const createProductItemElement = ({ sku, name, image }) => {
+  // FUNÇÃO QUE COLOCA OS PRODUTOS DINAMICAMENTE NO SITE PARA O CLIENTE COMPRAR
   const section = document.createElement('section');
   section.className = 'item';
-
+  
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -27,13 +28,32 @@ const createProductItemElement = ({ sku, name, image }) => {
   return section;
 };
 
-const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+// const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
+
+const sumCar = () => {
+  // FUNÇÃO PARA SOMAR O TOTAL DA COMPRA
+  const purchaseElem = document.querySelectorAll('.cart__item');
+  const totEspaco = document.querySelector('.total-price');
+  let totalSum = 0;
+  purchaseElem.forEach((elem) => {
+    const elemComp = parseFloat(elem.innerText.split('$')[1]);
+    totalSum += elemComp;
+    parseFloat(totalSum);
+    console.log(totalSum);
+  });
+  totEspaco.innerText = (totalSum);
+  
+  return totalSum;
+};
 
 const cartItemClickListener = (event) => {
+  // FUNÇÃO QUE REMOVE DO CARRINHO DE COMPRA, UNITARIAMENTE.
   event.target.remove();
+  sumCar();
 };
 
 const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
+  // FUNÇÃO QUE CRIA OS ELEMENTOS QUE FORAM COMPRADOS, ADICIONA NO CARRINHO
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -42,15 +62,18 @@ const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
 };
 
 const clicado = async (evento) => {
+  // FUNÇÃO COMPLEMENTAR PARA DAR AÇÃO AO EVENTO CLICAR
   const childFirst = (evento.target).parentNode;
   const idComprado = (childFirst.firstChild).textContent;
   const prodCar = await fetchItem(idComprado);
 
   const olCar = document.body.querySelector('.cart__items');
   olCar.appendChild(createCartItemElement(prodCar));
+  sumCar();
 };
 
 function captButt() {
+  // FUNÇÃO COMPLEMENTAR PARA ADICIONAR O EVENTO, CHAMANDO A FUNÇÃO CLICADO
   const btnAddCar = document.querySelectorAll('.item__add');
   btnAddCar.forEach((elem) => {
     elem.addEventListener('click', clicado);
@@ -58,6 +81,7 @@ function captButt() {
 }
 
 function carInic() {
+  // FUNÇÃO PARA ADICIONAR CARREGANDO EXERC.11
   const inicCarreg = document.querySelector('.items');
   const tagCarregando = document.createElement('p');
   tagCarregando.className = 'loading';
@@ -67,11 +91,13 @@ function carInic() {
 }
 
 function carEnd() {
+  // FUNÇÃO PARA REMOVER O CARREGANDO EXERC.11
   const tagEndCarreg = document.querySelector('.items').firstChild.remove();
   return tagEndCarreg;
 }
 
 const adicProdutos = async () => {
+  // FUNÇÃO DE ADICIONAR PRODUTO DA FETCH (ENDPOINT)
   carInic();
   const prodObjeto = await fetchProducts('computador');
   const sku = prodObjeto.results.map((elem) => {
@@ -92,8 +118,9 @@ const adicProdutos = async () => {
 async function clearCart() {
   // BOTÃO ESVAZIAR CARRINHO
   const carClearV = document.querySelector('.empty-cart');
-  await carClearV.addEventListener('click', () => {
+  carClearV.addEventListener('click', () => {
     document.querySelectorAll('.cart__item').forEach((e) => e.remove());
+    sumCar();
   });
 }
 
